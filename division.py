b9833aa21a5cf9c4bc5be2b494c434222a6d6ab3
Rodrigo C <rodricesp.3.0@gmail.com>
2022-03-28 18:38:26 -0400

    feat: operacion dividir

---
+++ division.py
@@ -0,0 +1,6 @@
+valor1 = float(input("Introduzca el primer valor:"))
+valor2 = float(input("Introduzca el segundo valor:"))
+division = valor1/valor2
+
+print("El resultado de la division es: ", division)
+print("el residuo es: ", valor1%valor2)
